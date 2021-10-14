cf0d581badec6d5811231b36759784a94d2f73c0
Mingyun (KJ) Kim <mingkim0210@gmail.com>
2021-10-13 21:30:59 -0700

    sign up and sign in

--- lambdas/handler.js
+++ lambdas/handler.js
@@ -7,15 +7,13 @@ const app = express();
 
 const USERS_TABLE = process.env.USERS_TABLE;
 const JOBPOSTING_TABLE = process.env.JOBPOSTING_TABLE;
+const JOBSEEKER_TABLE = process.env.JOBSEEKER_TABLE;
 const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
 
 app.use(express.json());
 
 app.get("/test", async function (req, res) {
-  res.json(
-  {
-    test: "Success"
-  });
+  res.json({ test: "Success" });
 });
 
 app.post("/addJobPosting", async function (req, res) {
@@ -54,8 +52,8 @@ app.post("/addJobPosting", async function (req, res) {
   } catch (error) {
     console.log(error);
     res.status(400).json({ 
-        error: "Could not create post",
-        message: error
+        message: "Could not create post",
+        error: error
       });
   }
 });
@@ -81,17 +79,104 @@ app.get("/getJobPostingByDistance", async function (req, res) {
           })
         );
     } else {
-      res.status(404).json({ error: "Could not retreive data" });
+      res.status(404).json({ message: "Could not retreive data" });
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: "Could not retreive data" });
+    res.status(500).json({ message: "Could not retreive data" });
+  }
+});
+
+app.post("/jobSeekerSignUp", async function (req, res) {
+  const {
+    userId,
+    firstName,
+    lastName,
+    email,
+    loginId,
+    password,
+    verified,
+    industry,
+    badge,
+    photoUrl,
+    buupCount,
+    skills,
+    socialMedia,
+    timestamp,
+    wageMin,
+    wageMax,
+    zipCode
+  } = req.body;
+  const params = {
+    TableName: JOBSEEKER_TABLE,
+    Item: {
+      userId: userId,
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      loginId: loginId,
+      password: password,
+      verified: verified,
+      insdustry: industry,
+      badge: badge,
+      photoUrl: photoUrl,
+      buupCount: buupCount,
+      skills: skills,
+      socialMedia: socialMedia,
+      timestamp: timestamp,
+      wageMin: wageMin,
+      wageMax: wageMax,
+      zipCode: zipCode
+    }
+  };
+  try {
+    await dynamoDbClient.put(params).promise();
+    console.log("data successfully uploaded");
+    res.status(200).json({ 
+      message: "data successfully uploaded",
+      data: params
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ 
+        message: "Could not create job seeker",
+        error: error
+      });
+  }
+});
+
+app.post("/jobSeekerSignIn", async function (req, res) {
+  const { loginId, password } = req.body;
+  const params = {
+    TableName: JOBSEEKER_TABLE,
+    KeyConditionExpression: 'loginId = :loginId',
+    ExpressionAttributeValues: {
+      ':loginId': loginId
+    }
+  };
+  try {
+    const Data = await dynamoDbClient.query(params).promise();
+    console.log(loginId, password);
+    console.log(Data.Items[0]);
+    if (Data && Data.Items[0].password === password) {
+      var userInfo = Data.Items[0];
+      userInfo.password = "";
+      res.status(200).json({ message: userInfo });
+    } else {
+      res.status(404).json({ message: "The info does not match our database" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Could not sign in",
+      error: error
+    });
   }
 });
 
 app.use((req, res, next) => {
   return res.status(404).json({
-    error: "Not Found",
+    message: "Not Found",
   });
 });
 
